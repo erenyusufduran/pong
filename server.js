dbbc99a4fbfc5cf1c80871ffeeb08cff929c6d6d
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-06-27 21:20:08 +0300

    using socket.io with express

--- server.js
+++ server.js
@@ -1,40 +1,20 @@
+const http = require('http');
+const io = require("socket.io");
 const cors = require("cors");
-const server = require("http").createServer();
-const io = require("socket.io")(server, {
+
+const apiServer = require("./api");
+const httpServer = http.createServer(apiServer);
+const socketServer = io(httpServer, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
 });
+const sockets = require("./sockets");
 
 const PORT = 3000;
-server.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
 
-let readyPlayerCount = 0;
-
-io.on("connection", (socket) => {
-  console.log("User connected with", socket.id);
-
-  socket.on("ready", () => {
-    console.log("Pleyer ready", socket.id);
-    readyPlayerCount++;
-    if (readyPlayerCount % 2 === 0) {
-      // broadcasting
-      io.emit("startGame", socket.id);
-    }
-  });
-
-  socket.on("paddleMove", (paddleData) => {
-    socket.broadcast.emit("paddleMove", paddleData);
-  });
-
-  socket.on("ballMove", (ballData) => {
-    socket.broadcast.emit("ballMove", ballData);
-  });
-
-  socket.on('disconnect', (reason) => {
-    console.log(`Client ${socket.id} disconnected: ${reason}`);
-  })
-});
+sockets.listen(socketServer);
---
+++ sockets.js
@@ -0,0 +1,32 @@
+let readyPlayerCount = 0;
+
+function listen(io) {
+  io.on("connection", (socket) => {
+    console.log("User connected with", socket.id);
+
+    socket.on("ready", () => {
+      console.log("Pleyer ready", socket.id);
+      readyPlayerCount++;
+      if (readyPlayerCount % 2 === 0) {
+        // broadcasting
+        io.emit("startGame", socket.id);
+      }
+    });
+
+    socket.on("paddleMove", (paddleData) => {
+      socket.broadcast.emit("paddleMove", paddleData);
+    });
+
+    socket.on("ballMove", (ballData) => {
+      socket.broadcast.emit("ballMove", ballData);
+    });
+
+    socket.on("disconnect", (reason) => {
+      console.log(`Client ${socket.id} disconnected: ${reason}`);
+    });
+  });
+}
+
+module.exports = {
+  listen,
+};
